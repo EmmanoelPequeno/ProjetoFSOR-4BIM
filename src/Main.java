751ccdd1d2fa83d8117e916a61eb459212fd880d
Mayllon Emmanoel Pequneo <92054592+EmmanoelPequeno@users.noreply.github.com>
2022-06-05 14:38:58 -0300

    Primeiro commit

---
+++ src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        /**
+        Ao iniciar a aplicação, ela deve iniciar um socket TCP na porta 8000 e se conectar no IP 127.0.0.1
+         */
+    }
+}
