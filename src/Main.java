c7e5b8b04d3e814935caf4157f1c5e403bd784ba
Mayllon Emmanoel Pequneo <92054592+EmmanoelPequeno@users.noreply.github.com>
2022-06-05 16:19:21 -0300

    Delete Main.java

--- src/Main.java
+++
@@ -1,7 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        /**
-        Ao iniciar a aplicação, ela deve iniciar um socket TCP na porta 8000 e se conectar no IP 127.0.0.1
-         */
-    }
-}
